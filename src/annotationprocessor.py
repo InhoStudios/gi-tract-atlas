c303088292654e42cbee254a5b55de0b604ed3bc
Andy Shen <andyshenproper@gmail.com>
2023-05-10 12:03:28 -0700

    Begin parsnig file structure for annotations

--- src/annotationprocessor.py
+++ src/annotationprocessor.py
@@ -1,10 +1,20 @@
 import json
+from structures import Atlas
 
-class ImageAnnotationProcessor:
+class AtlasProcessor:
     def __init__(self) -> None:
-        pass
+        self.atlas = Atlas()
     
     def addAnnotation(self, filepath):
         f = open(filepath)
-        json.load(f)
+        annotation = json.load(f)[0]
+
+        fname = annotation["documents"][0]["name"]
+        print(fname)
+
+        # parse name to get depth
+        # 
         f.close()
+
+    def viewAnnotation(self):
+        pass
--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
-from annotationprocessor import ImageAnnotationProcessor
+from annotationprocessor import AtlasProcessor
 
 from os.path import join
 
-annotationProcessor = ImageAnnotationProcessor()
-ANNOTATIONS_FOLDER = "../assets/annotations"
-IMAGES_FOLDER = "../assets/images"
+atlas = AtlasProcessor()
+ANNOTATIONS_FOLDER = ".\\assets\\annotations"
+IMAGES_FOLDER = ".\\assets\\images"
 
 if __name__=="__main__":
-    annotationProcessor.addAnnotation(join(ANNOTATIONS_FOLDER, "annotation1.json"))
+    atlas.addAnnotation(join(ANNOTATIONS_FOLDER, "annotation1.json"))
