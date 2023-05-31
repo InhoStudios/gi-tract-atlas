b51e4c0923bc8dafb552e8ab95282ee720f8477e
Andy Shen <andyshenproper@gmail.com>
2023-05-31 11:10:20 -0700

    Update calibration and smoothing to support new annotation

--- src/test.py
+++ src/test.py
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt
 
 if __name__=="__main__":
     atlas = Atlas(".\\assets\\annotations", ".\\assets\\annotations\\calibrate.json")
-    ax = plt.figure().add_subplot(projection='3d')
-    for organName in atlas.organs:
-        organ = atlas.organs[organName]
-        orgx, orgy, orgz = np.where(organ.voxelCloud > 0)
-        ax.scatter(orgx[0::500], orgy[0::500], orgz[0::500], label=organName)
-    ax.set_xlim(0, 600)
-    ax.set_ylim(0, 600)
-    ax.set_zlim(0, 600)
-    ax.legend()
-    plt.show()
+    # ax = plt.figure().add_subplot(projection='3d')
+    # for organName in atlas.organs:
+    #     organ = atlas.organs[organName]
+    #     orgx, orgy, orgz = np.where(organ.voxelCloud > 0)
+    #     ax.scatter(orgx[0::500], orgy[0::500], orgz[0::500], label=organName)
+    # ax.set_xlim(0, 600)
+    # ax.set_ylim(0, 600)
+    # ax.set_zlim(0, 600)
+    # ax.legend()
+    # plt.show()
